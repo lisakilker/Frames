3001378bd5755b8a66d25538aea18128945cb6d3
Lisa Kilker <lisakilker@gmail.com>
2024-02-07 16:46:56 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   async redirects(){
     return [
       {
-        source: '/undewater',
+        source: '/underwater',
         destination: 'https://www.bing.com/images/create',
         permanent: false
       },
