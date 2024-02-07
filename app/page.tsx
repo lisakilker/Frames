c9fe2a22eff33261f97c710a80fd3abb328991b4
Lisa Kilker <lisakilker@gmail.com>
2024-02-07 12:05:02 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const frameMetadata = getFrameMetadata({
       label: "View 7 Images"
     }
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmaBrMjQ85RGQp7Qk1Xd9HaBzXepWBk9K8mpqkc4dt3uwh/Image1.jpg`,
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmaBrMjQ85RGQp7Qk1Xd9HaBzXepWBk9K8mpqkc4dt3uwh/Image1.jpeg`,
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Create AI generated images',
     description: 'Create AI generated images',
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmaBrMjQ85RGQp7Qk1Xd9HaBzXepWBk9K8mpqkc4dt3uwh/Image1.jpg`],
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmaBrMjQ85RGQp7Qk1Xd9HaBzXepWBk9K8mpqkc4dt3uwh/Image1.jpeg`],
   },
   other: {
     ...frameMetadata,
