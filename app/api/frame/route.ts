72b221e4c4486b8ac6bd5fbc73017e2544480bd4
Lisa Kilker <lisakilker@gmail.com>
2024-02-07 15:30:18 -0600

    New frame

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -7,16 +7,16 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   const nextId = idAsNumber + 1
 
-  if(idAsNumber === 8){
+  if(idAsNumber === 7){
       return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>This is frame 8</title>
+    <title>This is frame 7</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmcRwetfaVHVwGrASvabTVoiX6tX6539SUeBWR6cfVUZ3v/Image8.jpeg" />
-    <meta property="fc:frame:button:1" content="Visit Bing.com to create" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmV71pbn9CBjZ5WS39EtfY9Rn15EuFLsN49xomQ29a8Txc/Image7.jpeg" />
+    <meta property="fc:frame:button:1" content="Create AI" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
-    <meta property="fc:frame:button:2" content="Learn more about Frames" />
+    <meta property="fc:frame:button:2" content="Learn Frames" />
     <meta property="fc:frame:button:2:action" content="post_redirect" />
-    <meta property="fc:frame:button:3" content="Create your own Frame" />
+    <meta property="fc:frame:button:3" content="Create a Frame" />
     <meta property="fc:frame:button:3:action" content="post_redirect" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
   </head></html>`);
@@ -24,7 +24,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmcRwetfaVHVwGrASvabTVoiX6tX6539SUeBWR6cfVUZ3v/Image${id}.jpeg" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmV71pbn9CBjZ5WS39EtfY9Rn15EuFLsN49xomQ29a8Txc/Image${id}.jpeg" />
     <meta property="fc:frame:button:1" content="Next Image" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
   </head></html>`);
